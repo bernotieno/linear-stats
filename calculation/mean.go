90ecf251a621f2e2852ea666c8cfaee7b6f7166a
bernaotieno <bernadokumu1@gmail.com>
2024-08-01 17:06:00 +0300

    add: a function that calculates the average of the data to be used in linear regression and finding the corelation coefficient

---
+++ calculation/mean.go
@@ -0,0 +1,11 @@
+package calculation
+
+func average(nums []float64) float64 {
+	total := 0.0
+	lenNum := len(nums)
+	for _, val := range nums {
+		total += val
+	}
+	mean := total / float64(lenNum)
+	return mean
+}
